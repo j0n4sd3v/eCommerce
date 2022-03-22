0f27641b20a4cc4312d61e691134fb09fd1f31f4
jonathan <contacto@jonasdev.es>
2022-03-22 12:31:50 +0100

    Product

---
+++ src/components/products/Products.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Grid from '@material-ui/core';
+import Product from './product/Product';
+
+const products=[
+    {id:1,name:"Shoes",description:"shoes for run",price:'€12'},
+    {id:2,name:"macBook",description:"portatil",price:'€30'}
+]
+const Products=()=>{
+    return(
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product)=>(
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product}/>
+                    </Grid>
+    ))}
+            </Grid>
+        </main>
+    )
+}
+export default Products;
